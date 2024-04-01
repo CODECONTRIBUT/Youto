644726b05f12fb0d15c3bcc198d6702de1661fe7
erin8 <erin8@ERIN>
2024-04-01 17:30:42 +1000

    1/04/2024 || Adjusted CSS of the contact panel. Installed React feather and added Icons.

--- src/components/ContactDetails.tsx
+++ src/components/ContactDetails.tsx
@@ -1,35 +1,40 @@
-import { Box, Grid, HStack, Heading, Link, SimpleGrid } from '@chakra-ui/react'
-//import * as Icon from 'react-feather';
+import { Box, Grid, HStack, Heading, Link, SimpleGrid, Text } from '@chakra-ui/react'
 import '../css/contactus.css'
+import * as Icon from 'react-feather';
 
 const ContactDetails = () => {
   return (
           <Box className='details_container'>
             <HStack className='text_container' display='flex' flexDirection='column'>
-              <Heading className='detailed_text'>Contact Information</Heading>
-              <Heading className='detailed_two'>Fill up the form and our team will get back to you within 24 hours</Heading>
+              <Heading className='detailed_one'>Contact Information</Heading>
+              <Text className='detailed_two'>Fill up the form and our team will get back to you within 24 hours</Text>
             </HStack>
-            <Box>
-              <Link href="tel:+123456789">
-                <Heading className='contact_text'>+123456789</Heading>
+            <Box className='contact_container'>
+              <Link className='contact_details' href="tel:+123456789" display='flex' alignItems='center'>
+                <Icon.Phone size={15} color="rgb(252, 113, 137)" />
+                <Text className='contact_text'>+123456789</Text>
               </Link>
       
-              <Link href="mailto:abc12345@gmail.com">
-                <Heading>abc12345@gmail.com</Heading>
+              <Link className='contact_details' href="mailto:abc12345@gmail.com" display='flex' alignItems='center'>
+                <Icon.Mail size={15} color="rgb(252, 113, 137)" />
+                <Text className='contact_text'>abc12345@gmail.com</Text>
               </Link>
             </Box>
       
-            <div>
+            <Box className='circle_container'>
               <div className='big_circle'></div>
               <div className='small_circle'></div>
-            </div>
+            </Box>
       
-            <SimpleGrid className='socialmedia_container'>
-              <Link className='social_icon' href="https://www.facebook.com/profile.php?id=100021937291259">
+            <SimpleGrid className='socialmedia_container' spacing={2} display='flex' flexDirection='row'>
+              <Link className='social_icon' href="https://www.facebook.com" target='_blank'>
+                <Icon.Facebook color="#fff" size={20} />
               </Link>
-              <Link className='social_icon' href="https://www.instagram.com/_allenjones/">
+              <Link className='social_icon' href="https://www.instagram.com" target='_blank'>
+                <Icon.Instagram color="#fff" size={20} />
               </Link>
-              <Link className='social_icon' href="https://www.linkedin.com/in/allen-jones-b799b7171/">
+              <Link className='social_icon' href="https://www.linkedin.com" target='_blank'>
+                <Icon.Linkedin color="#fff" size={20} />
               </Link>
             </SimpleGrid>
           </Box>
