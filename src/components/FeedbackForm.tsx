6fb2af171f0ab1327320a2c0a02da52ed84ea333
erin8 <erin8@ERIN>
2024-04-03 22:36:25 +1000

    3/04/2024 || Optimized the submit button and added sweet alert 2.

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Input} from '@chakra-ui/react';
 import { FieldValues, useForm } from 'react-hook-form';
 import { BiChevronUpCircle } from "react-icons/bi";
+import Swal from 'sweetalert2';
 
 interface FormData {
     name: string,
@@ -13,6 +14,11 @@ const FeedbackForm = () => {
     const {register, handleSubmit, reset, formState: {errors, isValid}} = useForm<FormData>();
     const onSubmit = (data: FieldValues) => {
         console.log(data);
+        Swal.fire({
+            title: "Thanks",
+            text: "Your message was sent through",
+            icon: "success"
+          });
         reset();
     };
 
@@ -20,24 +26,24 @@ const FeedbackForm = () => {
     <form className='input_form' onSubmit={handleSubmit(onSubmit)}>
             <Box className='input_block'>
                 <label htmlFor='name'>Name:</label>
-                <Input {...register('name', {required: true})} placeholder='Abc D' className='input_item'></Input>           
+                <Input {...register('name', {required: true})} className='input_item'></Input>           
             </Box>
-            {errors.name?.type === 'required' && <p color='red'>The name is required</p>}
+            {errors.name?.type === 'required' && <p className='error_msg'>The name is required</p>}
             <Box className='input_block'>
                 <label htmlFor='email'>Email:</label>
-                <Input {...register('email', {required: true})} placeholder='abc@gmail.com' className='input_item'></Input>           
+                <Input {...register('email', {required: true})} className='input_item'></Input>           
             </Box>
-            {errors.email?.type === 'required' && <p>The email is required</p>}
+            {errors.email?.type === 'required' && <p className='error_msg'>The email is required</p>}
             <Box className='input_block'>
                 <label htmlFor='phone'>Phone:</label>
-                <Input {...register('phone')}  placeholder='0412345678' className='input_item'></Input>
+                <Input {...register('phone')} className='input_item'></Input>
             </Box>
             <Box className='input_block'>
                 <label htmlFor='message'>Message:</label>
-                <Input {...register('message', {required: true})} placeholder='excellent!' className='input_item'></Input>
+                <Input {...register('message', {required: true})} className='input_item'></Input>
             </Box>
-            {errors.message?.type === 'required' && <p>Your message is required</p>}
-            <Button width='20%' disabled={!isValid} leftIcon={<BiChevronUpCircle />} colorScheme='teal' size='sm' fontWeight='bold' marginLeft={1} marginTop='25px' type='submit' variant='solid'>Submit</Button>     
+            {errors.message?.type === 'required' && <p className='error_msg'>Your message is required</p>}
+            <Button width='20%' isDisabled={!isValid} leftIcon={<BiChevronUpCircle />} colorScheme='teal' fontWeight='bold' marginLeft={1} marginTop='25px' type='submit' variant='solid'>Submit</Button>     
     </form>
   )
 }
