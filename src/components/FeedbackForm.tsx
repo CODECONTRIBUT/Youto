d06d010bb41669e452f32b83230fe05c1fe65a3b
erin8 <erin8@ERIN>
2024-04-03 12:01:24 +1000

    3/04/2024 || Adjusted css of the submit button.

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,6 +1,6 @@
-import { Box, Input, Text } from '@chakra-ui/react';
+import { Box, Button, Input} from '@chakra-ui/react';
 import { FieldValues, useForm } from 'react-hook-form';
-import { Button } from 'react-bootstrap';
+import { BiChevronUpCircle } from "react-icons/bi";
 
 interface FormData {
     name: string,
@@ -18,7 +18,6 @@ const FeedbackForm = () => {
 
   return (
     <form className='input_form' onSubmit={handleSubmit(onSubmit)}>
-        <Box className='input_group'>
             <Box className='input_block'>
                 <label htmlFor='name'>Name:</label>
                 <Input {...register('name', {required: true})} placeholder='Abc D' className='input_item'></Input>           
@@ -33,13 +32,12 @@ const FeedbackForm = () => {
                 <label htmlFor='phone'>Phone:</label>
                 <Input {...register('phone')}  placeholder='0412345678' className='input_item'></Input>
             </Box>
-            <Box className='input_block' marginBottom='30px'>
+            <Box className='input_block'>
                 <label htmlFor='message'>Message:</label>
                 <Input {...register('message', {required: true})} placeholder='excellent!' className='input_item'></Input>
             </Box>
             {errors.message?.type === 'required' && <p>Your message is required</p>}
-        </Box>
-        <Button disabled={!isValid} className='btn btn-primary submit-button' type='submit'>Submit</Button>
+            <Button width='20%' disabled={!isValid} leftIcon={<BiChevronUpCircle />} colorScheme='teal' size='sm' fontWeight='bold' marginLeft={1} marginTop='25px' type='submit' variant='solid'>Submit</Button>     
     </form>
   )
 }
