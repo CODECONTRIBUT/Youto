ae3fb43cb88ea77ae5a8c6ca879d202d14c35407
erin8 <erin8@ERIN>
2024-05-03 14:09:28 +1000

    3/05/2024 || Added a generic Select element.

---
+++ src/FormElements/SelectField.tsx
@@ -0,0 +1,38 @@
+import clsx from 'clsx';
+import * as React from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+import { FieldWrapper, FieldWrapperPassThroughProps } from './FieldWrapper';
+
+type Option = {
+  label: React.ReactNode;
+  value: string | number | string[];
+};
+
+type SelectFieldProps = FieldWrapperPassThroughProps & {
+  options: Option[];
+  className?: string;
+  defaultValue?: string;
+  registration: Partial<UseFormRegisterReturn>;
+};
+
+export const SelectField = ({label, options, error, className, defaultValue, registration}: SelectFieldProps) => {
+  return (
+    <FieldWrapper label={label} error={error}>
+      <select
+        className={clsx(
+          'mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-600 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md',
+          className
+        )}
+        defaultValue={defaultValue}
+        {...registration}
+      >
+        {options.map(({ label, value }) => (
+          <option key={label?.toString()} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </FieldWrapper>
+  );
+};
